66fa1d7d064634eb009e0cd283591c8b0b9e5451
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2022-01-22 18:40:33 +0200

    first-commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Блок 5*/
+$('.test').css('color', 'red');
+$('h2.test').css('color', 'red');
+$('p + p').css('color', 'red');
+$('li.www + li').css('color', 'red');
+$('h2').find('i').css('color', 'red');
+$('h2 > i').css('color', 'red');
+
+/*Блок 6*/
+$('h2:not(.test)').css('color', 'red');
+$(':header:not(.test)').css('color', 'red');
+$(':header' + ':header').css('color', 'red')
+$(':header.test' + ':header.test').css('color', 'red')
